db77034bee2df86b8fc4312786cc3f132d94c065
kkssbbb <03337@naver.com>
2023-02-02 17:07:03 +0900

    RGB거리 최소값을구할때 왜 min(m-1,n-1) +i번째 비용함?

--- seungbin/3week(DP)/1,23,더하기 3 reminde.js
+++ seungbin/3week(DP)/1,23,더하기 3 reminde.js
@@ -13,7 +13,7 @@ const inputMaxNum = Math.max(...inputArr) //10
 
 const arr = [0,1,2,4];// 0123 경우의 수
 
-for(let i =4; i<=inputMaxNum; i++){
+for(let i =4; i<=inputMaxNㅌum; i++){
     arr[i] = arr[i-1]+arr[i-2]+arr[i-3] %1000000009;
 }
 for(i of inputArr){
