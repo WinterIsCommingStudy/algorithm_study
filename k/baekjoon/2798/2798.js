6d0dee0db7726018513758d1d1d1d82391df1832
Win K <108077414+wink68@users.noreply.github.com>
2023-04-22 18:16:06 +0900

    Update 2798.js

--- k/baekjoon/2798/2798.js
+++ k/baekjoon/2798/2798.js
@@ -1,15 +1,26 @@
-const input = require("fs").readFileSync("/dev/stdin").toString().trim().split(/\s/).map(item => parseInt(item));
-const n = input[0];
-const m = input[1];
-const cards = input.slice(2);
-let sum = 0;
-for(i=0; i<cards.length-2; i++) {
-    for(j=i+1; j<cards.length-1; j++) {
-        for(k=j+1; k<cards.length; k++) {
-            if(cards[i]+cards[j]+cards[k] <= m && cards[i]+cards[j]+cards[k] > sum) {
-                sum = cards[i]+cards[j]+cards[k];
-            }
+// input값 처리
+const input = require('fs').readFileSync('/dev/stdin')
+  .toString().trim().split(/\s/).map((v) => +v);  // 정수화
+[N, M, ...cards] = input;
+
+console.log(solution(N, M, cards));
+
+// 블랙잭 최대값 메소드
+function solution(N, M, cards) {
+  let max = 0;
+
+  for (let i=0; i < N-2; i++) {      // 1번째 카드
+    for (let j=i+1; j < N-1; j++) {  // 2번째 카드
+      for (let k=j+1; k < N; k++) {  // 3번째 카드
+        let sum = cards[i] + cards[j] + cards[k];
+
+        if (M === sum) {
+          return sum;
+        } else if (max < sum && sum < M) {
+          max = sum;
         }
+      }
     }
+  }
+  return max;
 }
-console.log(sum);
