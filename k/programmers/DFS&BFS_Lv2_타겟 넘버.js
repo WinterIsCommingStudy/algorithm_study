587832dc69cf20a4878e077f3ca436866a267c91
Win K <108077414+wink68@users.noreply.github.com>
2023-05-17 07:35:24 +0900

    Update DFS&BFS_Lv2_타겟 넘버.js

--- k/programmers/DFS&BFS_Lv2_타겟 넘버.js
+++ k/programmers/DFS&BFS_Lv2_타겟 넘버.js
@@ -2,15 +2,17 @@ function solution(numbers, target) {
     let answer = 0;
     const len = numbers.length;
     
-    function dfs(cnt, sum) {
-        if (cnt === len) {
+    function dfs(idx, sum) {
+        // 1. 탈출 조건
+        if (idx === len) {        // 현재 인덱스가 배열 마지막 값에 왔을 때
             if (target === sum) {
                 answer++;
             }
             return;
         }
-        dfs(cnt + 1, sum + numbers[cnt]);
-        dfs(cnt + 1, sum - numbers[cnt])
+        // 2. 수행 동작
+        dfs(idx + 1, sum + numbers[idx]);  // 더할 경우
+        dfs(idx + 1, sum - numbers[idx]);  // 뺄 경우
     }
     dfs(0, 0);
     return answer;
