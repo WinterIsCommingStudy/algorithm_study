6bdda298a2e2291af6ae6cb08de5e1f324569397
Win K <108077414+wink68@users.noreply.github.com>
2023-05-25 10:15:04 +0900

    Update Lv1_완주하지_못한_선수.js

--- k/programmers/해시/Lv1_완주하지_못한_선수.js
+++ k/programmers/해시/Lv1_완주하지_못한_선수.js
@@ -3,7 +3,7 @@ function solution(participant, completion) {
     participant.sort();
     completion.sort();
     
-    for (let i=0; participant.length; i++) {
+    for (let i=0; i < participant.length; i++) {
         if (participant[i] !== completion[i]) {
             return participant[i];
         }
