540a6a5505a733d2f702bd5a9509d57690e508e3
Win K <108077414+wink68@users.noreply.github.com>
2023-06-29 04:51:12 +0900

    Update Lv2_튜플.js

--- k/programmers/Lv2_튜플.js
+++ k/programmers/Lv2_튜플.js
@@ -1,15 +1,15 @@
 function solution(s) {
     const answer = [];
-    const tuple = s.slice(2, -2)
-                 .split("},{")
-                 .sort((a,b) => a.length - b.length)
-                 .map((item) => item.split(",").map(Number))
-                 .map((arr) => {
-                     arr.map((num) => {
-                         if(!answer.includes(num)) {
-                             answer.push(num)
-                         }
-                     })
-                 });
+    s.slice(2, -2)
+      .split("},{")
+      .sort((a,b) => a.length - b.length)
+      .map((item) => item.split(",").map(Number))
+      .map((arr) => {
+        arr.map((num) => {
+            if(!answer.includes(num)) {
+                answer.push(num)
+            }
+        })
+      });
     return answer;
 }
